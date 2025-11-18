1531ab4ecdcd6d9c8047a9bbb345204843592054
MochammadAby <isfamaulanaabi1@gmail.com>
2025-11-18 18:58:56 +0700

    .

--- Project PBO - FarMoo/Controllers/AuthController.cs
+++ Project PBO - FarMoo/Controllers/AuthController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Npgsql;
+using Project_PBO___FarMoo.Database;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,31 @@ namespace Project_PBO___FarMoo.Controllers
 {
     internal class AuthController
     {
+        private DbContext _db = new DbContext();
+
+        public bool Login(string user, string pass)
+        {
+            _db.Open();
+
+            string query = "SELECT * FROM users WHERE username=@u AND password=@p";
+
+            using (var cmd = new NpgsqlCommand(query, _db.Connection))
+            {
+                cmd.Parameters.AddWithValue("@u", user);
+                cmd.Parameters.AddWithValue("@p", pass);
+
+                using (var reader = cmd.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        _db.Close();
+                        return true;
+                    }
+                }
+            }
+
+            _db.Close();
+            return false;
+        }
     }
 }
--- Project PBO - FarMoo/Database/DbContext.cs
+++ Project PBO - FarMoo/Database/DbContext.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DotNetEnv;
+using Npgsql;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,8 +15,11 @@ namespace Project_PBO___FarMoo.Database
         private string _dbUser;
         private string _dbPassword;
         private string _dbName;
+        private string _sslMode;
+        private string _channelBinding;
 
         public string connStr;
+        public NpgsqlConnection Connection;
 
         public DbContext()
         {
@@ -25,9 +30,31 @@ namespace Project_PBO___FarMoo.Database
             _dbUser = Environment.GetEnvironmentVariable("DB_USER");
             _dbPassword = Environment.GetEnvironmentVariable("DB_PASSWORD");
             _dbName = Environment.GetEnvironmentVariable("DB_NAME");
+            _sslMode = Environment.GetEnvironmentVariable("SSL_MODE") ?? "Disable";
+            _channelBinding = Environment.GetEnvironmentVariable("CHANNEL_BINDING") ?? "Disable";
 
-            connStr = $"Host={_dbHost};Port={_dbPort};Username={_dbUser};Password={_dbPassword};Database={_dbName}";
+            connStr =
+                $"Host={_dbHost};" +
+                $"Port={_dbPort};" +
+                $"Username={_dbUser};" +
+                $"Password={_dbPassword};" +
+                $"Database={_dbName};" +
+                $"SSL Mode={_sslMode};" +
+                $"ChannelBinding={_channelBinding};";
 
+            Connection = new NpgsqlConnection(connStr);
+        }
+
+        public void Open()
+        {
+            if (Connection.State != System.Data.ConnectionState.Open)
+                Connection.Open();
+        }
+
+        public void Close()
+        {
+            if (Connection.State != System.Data.ConnectionState.Closed)
+                Connection.Close();
         }
     }
 }

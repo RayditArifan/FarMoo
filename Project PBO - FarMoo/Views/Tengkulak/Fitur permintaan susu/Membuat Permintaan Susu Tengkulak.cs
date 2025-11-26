c605b0f51827f9cdeeb096b9d7e1a57ccc9204d8
RayditArifan <raditya.bwi15@gmail.com>
2025-11-26 17:05:52 +0700

    hai

---
+++ Project PBO - FarMoo/Views/Tengkulak/Fitur permintaan susu/Membuat Permintaan Susu Tengkulak.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_PBO___FarMoo.Views.Tengkulak.Fitur_permintaan_susu
+{
+    public partial class Membuat_Permintaan_Susu_Tengkulak : Form
+    {
+        public Membuat_Permintaan_Susu_Tengkulak()
+        {
+            InitializeComponent();
+        }
+    }
+}
